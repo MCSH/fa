68f0bafbf8c9eaf5bab9ea49381a25658ba10fad
MCSH <mcshemail@gmail.com>
2017-07-14 16:54:02 +0430

    added new post

---
+++ new_post.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+NAME=_drafts/$(date -I)-$1.markdown
+touch $NAME
+
+p(){
+    echo $@ >> $NAME
+}
+
+p ----
+p title: $1
+p date: $(date -Is | awk 'gsub("T", " ")' | awk 'gsub("+", " +")') 
+p categories: 
+p ----
+p some text
+p
+p "{% highlight python %}"
+p 'if __name__ == "__main__":'
+p '  print "Hello World!"'
+p "{% endhighlight %}"
+
+echo $NAME
